a2541bd6e2cff3f3a750101919c86537a9987a03
nikitaCompany <isvesar@gmail.com>
2018-06-24 21:19:20 +0300

    Add class for string constant and make module for online check

---
+++ app/WordConstant.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nikita
+ * Date: 24.06.2018
+ * Time: 21:05
+ */
+
+namespace App;
+
+
+class WordConstant
+{
+
+    const STATUS_OK = "OK";
+    const STATUS_FAIL = "Ошибка";
+    const COLOR_OK = '00FF00';
+    const COLOR_FAIL = 'FF0000';
+
+    // ROBOTS constants //
+    const ROBOTS_EXIST_TEXT = "Проверка наличия файла robot.txt";
+    // ROBOTS constants //
+
+    // HOST constants //
+    const HOST_EXIST_TEXT = "Проверка указания директивы Host";
+    const TEXT_FAIL_ALL = "Опаньки :) Проверка невозможна, так как директива Host отсутствует";
+    // HOST constants //
+}
